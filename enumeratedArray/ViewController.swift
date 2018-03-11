cb121cd243b8cead3da900a4de87276f0faca48e
Derrick Showers <derrick@derrickshowers.com>
2018-03-11 16:26:52 -0700

    setup

--- enumeratedArray/ViewController.swift
+++ enumeratedArray/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
+enum MockEnum: Int {
+
+    case Foo
+    case Bar
+    case Baz
+
+    var name: String {
+        switch self {
+        case .Foo:
+            return "foo"
+        case .Bar:
+            return "bar"
+        case .Baz:
+            return "baz"
+        }
+    }
+}
+
+struct SomeData {
+    let isFirst: Bool
+    let name: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let cases = buildArray()
+        let data = enumerateMockCases(cases)
+        print("=============FINAL RESULT==============")
+        print(data.map({ $0.name }))
+        print("=======================================")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func buildArray() -> [MockEnum] {
+
+        var values = [MockEnum]()
+        var index = 1
+
+        while let element = MockEnum(rawValue: index), element.hashValue != 0 {
+            values.append(element)
+            index += 1
+        }
+
+        return values
     }
 
-
+    private func enumerateMockCases(_ cases: [MockEnum]) -> [SomeData] {
+        return cases.enumerated().flatMap { (index, element) in
+            print("============================")
+            print("Index: \(index)")
+            print("Mock: \(element)")
+            print("Mapping...")
+            print("============================")
+            let isFirst = index == 0
+            return SomeData(isFirst: isFirst, name: element.name)
+        }
+    }
 }
 
